e280ac360be1890a53370a2c34d4d2f93c55aaac
X-Anas-X <anas.zain1996@gmail.com>
2020-04-11 18:09:41 +0300

    done

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE location(
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude json,
-    longitude json
+    latitude NUMERIC(10, 7),
+    longitude NUMERIC(10, 7)
 );
 -- INSERT INTO citys (search_query, formatted_query, latitude, longitude ) VALUES ('hi', 'amman', );
 
